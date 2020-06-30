ef07fd30393c0b871b040025f062cf5513674083
Joseph Massabni <46639729+josephmassabni@users.noreply.github.com>
2020-06-30 18:23:45 -0500

    Add files via upload

---
+++ Personal Website/js/custom.js
@@ -0,0 +1,7 @@
+//For Video background
+var videobackground = document.getElementById("videobackground");
+
+function videoBackgroundPlay() {
+  videobackground.load();
+  videobackground.play();
+}
